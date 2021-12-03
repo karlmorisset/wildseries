3ff07ad0387a3a11f8f6167cc4f1d389d25b6c0f
Karl Morisset <karl.morisset.register@gmail.com>
2021-12-03 20:47:39 +0100

    Quête 9 ok

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Season;
 use App\Entity\Program;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -38,12 +39,49 @@ class ProgramController extends AbstractController
     {
         $program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $id]);
 
+        $seasons = $program->getSeasons();
+
         if (!$program) {
             throw $this->createNotFoundException(
                 "Aucune série avec l'identifiant {$id} n'existe dans la base de données"
             );
         }
 
-        return $this->render('program/show.html.twig', ['program' => $program]);
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+            'seasons' => $seasons
+        ]);
+    }
+
+
+    /**
+     * Récupère une série par son id
+     *
+     * @Route("/{programId<^[0-9]+$>}/season/{seasonId<^[0-9]+$>}", methods={"GET"}, name="season_show")
+     * @return Response
+     */
+    public function showSeason(int $programId, int $seasonId): Response
+    {
+        $program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $programId]);
+        $season = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['id' => $seasonId]);
+        $episodes = $season->getEpisodes();
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                "Aucune série avec l'identifiant {$programId} n'existe dans la base de données"
+            );
+        }
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                "Aucune saison avec l'identifiant {$seasonId} n'existe dans la base de données"
+            );
+        }
+
+        return $this->render('program/season_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episodes' => $episodes
+        ]);
     }
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,17 @@ class Category
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Program::class, mappedBy="category")
+     */
+    private $program;
+
+    public function __construct()
+    {
+        $this->program = new ArrayCollection();
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +48,36 @@ class Category
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Program[]
+     */
+    public function getProgram(): Collection
+    {
+        return $this->program;
+    }
+
+    public function addProgram(Program $program): self
+    {
+        if (!$this->program->contains($program)) {
+            $this->program[] = $program;
+            $program->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProgram(Program $program): self
+    {
+        if ($this->program->removeElement($program)) {
+            // set the owning side to null (unless already changed)
+            if ($program->getCategory() === $this) {
+                $program->setCategory(null);
+            }
+        }
 
         return $this;
     }
