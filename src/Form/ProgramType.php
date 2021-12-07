8217446041bb81ae841aa5cc943e55924a07ebf2
Karl Morisset <karl.morisset.register@gmail.com>
2021-12-08 00:59:48 +0100

    Quête 16 ok

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProgramType extends AbstractType
@@ -16,7 +18,13 @@ class ProgramType extends AbstractType
             ->add('synopsis')
             ->add('poster')
             ->add('category', null, ['choice_label' => 'name'])
-        ;
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
