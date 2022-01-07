30e20d1613b0502806ad885541d5e2edb9f64e8a
[WCS/NTS] Karl Morisset <90033508+karlmorisset@users.noreply.github.com>
2022-01-07 23:45:38 +0100

    Quête 27 ok

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,4 +32,19 @@ class DefaultController extends AbstractController
 
         return $this->redirect($referer);
     }
+
+
+    /**
+     * Permet de rendre une vue spécifique pour une dropdown des catégories
+     *
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+       return $this->render('layout/_navbartop.html.twig', [
+          'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+       ]);
+    }
+
 }
